4239499275d1417b62e54ed4191b2a116082bce8
Philip <philipcho101@hotmail.com>
2022-05-15 22:26:17 -0700

    Add operate calls when operators are pressed

--- javascript.js
+++ javascript.js
@@ -18,6 +18,8 @@ const divide = function(a, b) {
 };
 
 const operate = function(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
     if (operator == '+') {
         return add(a, b);
     }
@@ -32,18 +34,54 @@ const operate = function(operator, a, b) {
     }
 }
 
-let displayValue = '';
+let displayValue = '0';
+let storedValue = '';
+let operator = '';
+let prevKey = '';
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        if (e.target.textContent == 'AC') {
-            displayValue = '';
+        if (e.target.className == 'clear') {
+            displayValue = '0';
+            storedValue = '';
+            operator = '';
+            prevKey = '';
             display.textContent = displayValue;
         }
-        else {
-            displayValue += e.target.textContent
+        else if (e.target.className == 'operator') {
+            if (prevKey == 'operator')
+            {
+                operator = e.target.textContent;
+            }
+            else if (prevKey == 'equals') {
+                storedValue = displayValue;
+                operator = e.target.textContent;
+                displayValue = '0';
+                prevKey = 'operator';
+            }
+            else {
+                if (storedValue != '') {
+                    displayValue = operate(operator, storedValue, display.textContent);
+                    display.textContent = displayValue;
+                }
+                storedValue = displayValue;
+                operator = e.target.textContent;
+                displayValue = '0';
+                prevKey = 'operator';
+            }
+        }
+        else if (e.target.className == 'equals' && prevKey != 'equals' && prevKey != 'operator') {
+            displayValue = operate(operator, storedValue, displayValue);
+            operator = '';
+            storedValue = displayValue;
             display.textContent = displayValue;
+            prevKey = 'equals';
+        }
+        else if (e.target.className == 'digit') {
+            displayValue += e.target.textContent;
+            display.textContent = displayValue;
+            prevKey = 'num';
         }
     });
 });
